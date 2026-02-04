fc55e9a0a8899227a9a5f948f98c34ff8ff7047c
Zynh Ludwig <zynh0722@gmail.com>
2026-02-03 18:42:00 -0800

    catch revlib

--- src/main/java/frc/robot/subsystems/drivebase/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/drivebase/SwerveModule.java
@@ -7,8 +7,8 @@ package frc.robot.subsystems.drivebase;
 import com.ctre.phoenix6.hardware.CANcoder;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.spark.SparkMax;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkMaxConfig;
 
