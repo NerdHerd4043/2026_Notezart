18a7735bde9ae1c5fa3a90861d16a43d29cf7122
Zynh Ludwig <zynh0722@gmail.com>
2026-05-16 17:28:51 -0700

    cleanup: intake dead code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -53,10 +53,6 @@ public class Intake extends SubsystemBase {
     kickupMotor.stopMotor();
   }
 
-  // public boolean getIntakeOn() {
-  // return intakeMotor.get
-  // }
-
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
